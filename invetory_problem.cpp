70d0300d5c28ee169845bdc881fe6539a4040c42
Parag Pathak <31978917+paragpathak2006@users.noreply.github.com>
2023-07-10 12:09:06 -0400

    Update invetory_problem.cpp

--- invetory_problem.cpp
+++ invetory_problem.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <unordered_map>
+#include <unordered_set>
 #include <algorithm>
 #include <execution>
 
@@ -24,6 +25,10 @@ public:
 
     Point operator+(const Point& P) const {
         return Point(x + P.x, y + P.y, z + P.z);
+    }
+
+    bool operator==(const Point& P) const {
+        return x == P.x && y == P.y && z == P.z;
     }
 
     Point operator-(const Point& P) const {
@@ -64,42 +69,68 @@ public:
         return P0.ySegment(p[P[0]], p[P[1]]) == 1 && P0.ySegment(p[P[1]], p[P[2]]) == 1;
     }
 
-    int PMCz_cross_product_test(const Point& P0, const Points& p) const {
+    string zIntercept(const Point P0, const Points& p) const {
+        const Point& Pref0 = p[P[0]] - P0;
+        // Face intersection
+        if (normal.z > 0) {
+            double z = normal.z * (normal.x * Pref0.x + normal.y * Pref0.y + normal.z * Pref0.z);
+            if (z > 0) return "z_Positive intersection";
+            if (z < 0) return "z_Negative intersection";
+            return "z_on_Body";
+        }
+
+        // Tetra volume
+        if(tetraVol(p[P[0]], p[P[1]], p[P[2]],P0)==0)
+            return "z_on_Body";
+    }
+
+    string zAxisIntersection(const Point P0, const Points& p, unordered_set<int>& pList) const {
+        // check X,Y bounding box for face.
+        if (checkXAxisIntersection(P0, p)) return "No_Intersection";
+        if (checkYAxisIntersection(P0, p)) return "No_Intersection";
+
+        // Check Point intersections
+        for (size_t i = 0; i < 3; i++)
+        {
+            if (P0.x == p[P[i]].x && P0.y == p[P[i]].y && p[P[i]].isProjection()) {
+                if (pList.count(P[i]) == 0) {
+                    pList.emplace(P[i]);
+                    if (p[P[i]].z > P0.z) "z_Positive intersection";
+                    if (p[P[i]].z < P0.z) "z_Negative intersection";
+                }
+                return "No_Intersection";
+            }
+        }
+
+        // Check Edge intersections
         double area[3];
         for (size_t i = 0; i < 3; i++) {
             const Point& Pi = p[P[i]];
             const Point& Pj = p[P[next(i)]];
             area[i] = (Pi - P0) * (Pj - P0);
-            if (tol(area[i], 0)) 
-                return 0;
+            if (area[i] == 0) {
+                if (P[i] < P[next(i)] && !EDGE(P[i] , P[next(i)]).isProjection())
+                    return "edge_intersection";
+                else
+                    return "no_intersection";
+            }
         }
 
-        if (area[0] > 0 && area[1] > 0 && area[2] > 0) return 1;
-        if (area[0] < 0 && area[1] < 0 && area[2] < 0) return 1;
-        return -1;
-    }
+        if (area[0] > 0 && area[1] > 0 && area[2] > 0) return "no_intersection";
+        if (area[0] < 0 && area[1] < 0 && area[2] < 0) return "no_intersection";
 
-    int zIntercept(const Point P0, const Points& p) const {
-        const Point& Pref = p[P[0]] - P0;
-        double z = normal.z * (normal.x * Pref.x + normal.y * Pref.y + normal.z * Pref.z);
-        if (z > 0) return 1;
-        if (z < 0) return -1;
-        return 2;
-    }
-    int PMCz(const Point P0, const Points& p) const {
-        //Most faces would resolve in the axis in tersection tests.
-        if (checkXAxisIntersection(P0, p)) return 0;
-        if (checkYAxisIntersection(P0, p)) return 0;
 
         // check Axis was inconclusive, returning cross product test
-        if (PMCz_cross_product_test(P0, p) == 1 ) return 0;
+        if (PMCz_cross_product_test(P0, p) == "no_intersection") 
+            return "No_Intersection";
 
+        // Check Face intersections
         return zIntercept(P0, p);
     }
 };
 
 typedef vector<Face> Faces;
-int PMC( const Faces& faces, const Points& points, const Point& P ) {
+int PMC( const Faces& faces, const Points& points, const Point& P, unordered_set<int>& pList) {
     std::atomic<int> z_positve = 0, z_negative = 0;
     bool notOnBody = true;
 
@@ -109,10 +140,10 @@ int PMC( const Faces& faces, const Points& points, const Point& P ) {
         faces.end(),
         [&P, &points, &z_positve, &z_negative, &notOnBody] ( const Face& face) {
             if (notOnBody) {
-                auto res = face.PMCz(P, points);
-                if (res == 2)                    notOnBody = false;
-                if (res == 1)                    z_positve++;
-                if (res == -1)                   z_negative++;
+                auto res = face.zAxisIntersection(P, points, pList);
+                if (res == "z_on_Body")                     notOnBody = false;
+                if (res == "z_Positive intersection")                              z_positve++;
+                if (res == "z_Negative intersection")                              z_negative++;
             }
         });
 
@@ -126,10 +157,43 @@ int main()
     Faces faces;
     Points points;
     Point P(0, 0, 0);
-    
-    auto res = PMC(faces, points, P);
+    unordered_set<int> pList;
+    auto res = PMC(faces, points, P, pList);
     if (res == 1) cout << "Outside the body";
     if (res == 0) cout << "On the body";
-    if (res == 1) cout << "Inside the body";
+    if (res == -1) cout << "Inside the body";
     return 0;
 }
+//int test(vector<int> v) {
+//
+//    vector<int> indices(v.size(), 0);
+//
+//    int len = v.size(), sum = 0, imax = len - 1, imin = 0;
+//
+//    for (size_t i = 0; i < len; i++)
+//        indices[i] = i;
+//
+//    sort(indices.begin(), indices.end(), &[v](int i, int j) { return v[i] <= v[j]; });
+//
+//    for (size_t i = 0; i < len; i++) {
+//        int index = indices[i];
+//        if (v[index] != 0) {
+//            sum = sum + v[index];
+//            if (index != imax) v[index + 1] = 0;
+//            if (index != imin) v[index - 1] = 0;
+//        }
+//    }
+//
+//    return sum;
+//}
+//
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
